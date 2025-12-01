cd978fc3f790bfc0a76378f751a7d420318fded5
abdullahbelli <belliabdullah@gmail.com>
2025-12-01 22:50:37 +0300

    İlkProjeVisual klasörünü normal proje klasörü yaptım

---
+++ İlkProjeVisual/İlkProjeVisual/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace İlkProjeVisual
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İlk adımımız");
+        }
+    }
+}
+
+// Console.WriteLine("İlk adımımız");
+// Sadece bunu yazarsak top-level statement bunu main olarak algılar ve çalışır

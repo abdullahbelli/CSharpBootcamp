a8d66ce590037e3662eecd4493cdae2a3a1571f7
abdullahbelli <belliabdullah@gmail.com>
2025-12-29 21:30:42 +0300

    Eksik günler tamamlandı. Son gün eklendi.

---
+++ FilmKoleksiyonu/FilmKoleksiyonu/FilmListesi.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FilmKoleksiyonu
+{
+    public class FilmListesi
+    {
+        /*
+         * Bu nesne oluştuğunda, varsayılan olarak birkaç film koleksiyonda olsun.
+         */
+
+        private List<Film> filmler= new List<Film>();
+
+        public FilmListesi() {
+            varsayilanFilmleriEkle();
+        }
+
+        private void varsayilanFilmleriEkle()
+        {
+            try
+            {
+                filmler.Add(new Film("Matrix","Watchowski Kardeşler", 1999, 9.4,"Bilim Kurgu"));
+                filmler.Add(new Film("Fargo","Joel Coen", 1997, 8,"Karanlık Komedi"));
+                filmler.Add(new Film("Pulp Fiction","Quentin Tarantino", 1994, 8,"Suç-Komedi"));
+                filmler.Add(new Film("Interstellar","Christopher Nolan", 2014, 8.2,"Bilim Kurgu"));
+                filmler.Add(new Film("The Contact","Robert Zemeckis", 1997, 8.6,"Bilim Kurgu"));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        // 1. Film ekleme
+        public void Ekle(Film film)
+        {
+            // 1. Film nesnesi boş olamaz!!!
+            ArgumentNullException.ThrowIfNull(film);
+
+            filmler.Add(film);
+            Console.WriteLine($"{film.Ad} isimli film başarıyla eklendi");
+        }
+
+        // 2. Arama
+
+        private void listeKontrol()
+        {
+            if (filmler.Count == 0)
+            {
+                throw new InvalidOperationException("filmler koleksiyonu boş!!!");
+            }
+        }
+
+        public List<Film> AdaGoreAra(string filmAdi)
+        {
+            // 1. Liste boşsa aramaya gerek yok!
+            listeKontrol();
+
+            List<Film> bulunanFilmler = new List<Film>();
+
+            foreach (var film in filmler)
+            {
+                if (film.Ad.ToLower() == filmAdi.ToLower())
+                {
+                    bulunanFilmler.Add(film);
+                }
+            }
+
+            return bulunanFilmler;
+
+
+        }
+
+        public List<Film> YilaGoreAra(int yil)
+        {
+            // 1. Liste boşsa aramaya gerek yok!
+            listeKontrol();
+
+            List<Film> bulunanFilmler = new List<Film>();
+
+            foreach (var film in filmler)
+            {
+                if (film.Yil == yil)
+                {
+                    bulunanFilmler.Add(film);
+                }
+            }
+
+            return bulunanFilmler;
+
+
+        }
+
+        public List<Film> TureGoreAra(string tur)
+        {
+            // 1. Liste boşsa aramaya gerek yok!
+            listeKontrol();
+
+            List<Film> bulunanFilmler = new List<Film>();
+
+            foreach (var film in filmler)
+            {
+                if (film.Tur == tur)
+                {
+                    bulunanFilmler.Add(film);
+                }
+            }
+
+            return bulunanFilmler;
+
+
+        }
+
+        public Film EnYuksekPuanlıFilmiBul()
+        {
+            // 1. Liste boşsa aramaya gerek yok!
+            listeKontrol();
+
+            Film enIyiFilm = filmler[0];
+            foreach (var film in filmler)
+            {
+                if (film.Puan > enIyiFilm.Puan)
+                {
+                    enIyiFilm = film;
+                }
+            }
+
+            return enIyiFilm;
+        }
+
+        //Ortalama puan hesaplama
+        public double ortalamaPuan()
+        {
+            listeKontrol();
+
+            double toplam = 0;
+            foreach (var film in filmler)
+            {
+                toplam += film.Puan;
+            }
+            return toplam/filmler.Count;
+        }
+
+        public List<Film> TumFilmleriGetir()
+        {
+            listeKontrol();
+            List<Film> kopyaListe = new List<Film>(filmler);
+            return kopyaListe;
+        }
+
+
+    }
+}
---
+++ FilmKoleksiyonu/FilmKoleksiyonu/Program.cs
@@ -0,0 +1,86 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FilmKoleksiyonu;
+
+Console.WriteLine("Hello, World!");
+/*
+*   Filmleri bir arda tutan ve çeşitli işlevlere sahip bir uygulama geliştirmek istiyoruz.
+*       İşlevler:
+*           1. Film ekleme
+*           2. Arama
+*           3. Filtreleme (yıla, türe göre ...)
+*           4. En yüksek puanlı filmi bulma
+*           5. Ortalama puan hesaplama
+*   
+*   
+*   uygulama başladığında bir menü gelir. Bu menüye göre kullanıcı seçim yaparak ilerler.
+*/
+
+// Nesne testi:
+/*try
+{
+    Film inception = new Film("Inception","Christopher Nolan",2010,7.5,"Bilim Kurgu");
+    Console.WriteLine("Film nesnesi oluşturuldu.");
+    Console.WriteLine(inception);
+
+    Film film2 = new Film("Test Film","Test",2020,15,"Aksiyon");
+
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex.Message);
+    
+}
+*/
+
+bool devamEtsinMi = true;
+FilmListesi filmListesi = new FilmListesi();
+while (devamEtsinMi)
+{
+    Menu.Goster();
+    Console.WriteLine("Seçiminiz");
+    string secim =  Console.ReadLine();
+    
+
+    switch (secim)
+    {
+        case "1":
+            Menu.FilmEkle(filmListesi);
+            break;
+
+        case "2":
+            Menu.AdaGoreFilmAra(filmListesi);
+            break;
+        case "3":
+            Menu.YilaGoreFilmAra(filmListesi);
+            break;
+        case "4":
+            Menu.TureGoreFilmAra(filmListesi);
+            break;
+        case "5":
+            Menu.TumFilmleriListele(filmListesi);
+            break;
+        case "6":
+            Menu.EnİyiFilm(filmListesi);
+            break;
+        case "7":
+            Menu.OrtalamaPuan(filmListesi);
+            break;
+        case "8":
+            Menu.Cikis();
+            devamEtsinMi = false;
+            break;
+        default:
+            break;
+    }
+
+    if (devamEtsinMi)
+    {
+        Console.WriteLine("Devam etmek için bir tuşa basın...");
+        Console.ReadKey();
+        Console.Clear();
+        
+    }
+}
+
+
